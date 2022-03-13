17d4a516ca2e6ac5e87110af51cc37e486dfd717
有種 <39034242+kenzok8@users.noreply.github.com>
2022-03-13 21:11:43 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+function git_sparse_clone() (
+branch="$1" rurl="$2" localdir="$3" && shift 3
+git clone -b $branch --depth 1 --filter=blob:none --sparse $rurl $localdir
+cd $localdir
+git sparse-checkout init --cone
+git sparse-checkout set $@
+mv -n $@ ../
+cd ..
+rm -rf $localdir
+
 function mvdir() {
 mv -n `find $1/* -maxdepth 0 -type d` ./
 rm -rf $1
