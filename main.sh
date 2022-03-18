adc957288dee6b96301952c9be541a479471eb7b
有種 <39034242+kenzok8@users.noreply.github.com>
2022-03-18 19:50:23 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -109,6 +109,7 @@ svn co https://github.com/messense/aliyundrive-fuse/trunk/openwrt && mvdir openw
 svn co https://github.com/messense/openwrt-wiretrustee/trunk/wiretrustee
 svn co https://github.com/xiaorouji/openwrt-passwall2/trunk/luci-app-passwall2
 svn co https://github.com/Ysurac/openmptcprouter-feeds/trunk/luci-app-snmpd
+svn co https://github.com/immortalwrt/luci/trunk/applications/luci-app-filebrowser
 
 git_sparse_clone master "https://github.com/coolsnowwolf/packages" "leanpkg" net/miniupnpd net/mwan3 multimedia/UnblockNeteaseMusic-Go \
 multimedia/UnblockNeteaseMusic net/amule net/baidupcs-web multimedia/gmediarender net/go-aliyundrive-webdav \
