458ed5939bca1755f3b3a6dc568e0abc616d2b6c
有種 <39034242+kenzok8@users.noreply.github.com>
2022-04-06 10:50:49 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
-shopt -s extglob
-git rm -r --cache * >/dev/null 2>&1 &
+shopt -s extglobgit rm -r --cache * >/dev/null 2>&1 &
 rm -rf `find ./* -maxdepth 0 -type d ! -name "diy"` >/dev/null 2>&1
-function git clone() {
-  git clone --depth 1 $1 $2 || true
-{
 function git_sparse_clone() {
-  branch="$1" rurl="$2" localdir="$3" && shift 3
-  git clone -b $branch --depth 1 --filter=blob:none --sparse $rurl $localdir
-  cd $localdir
-  git sparse-checkout init --cone
-  git sparse-checkout set $@
-  mv -n $@ ../
-  cd ..
-  rm -rf $localdir
-  }
+branch="$1" rurl="$2" localdir="$3" && shift 3
+git clone -b $branch --depth 1 --filter=blob:none --sparse $rurl $localdir
+cd $localdir
+git sparse-checkout init --cone
+git sparse-checkout set $@
+mv -n $@ ../
+cd ..
+rm -rf $localdir
+}
 function mvdir() {
 mv -n `find $1/* -maxdepth 0 -type d` ./
 rm -rf $1
