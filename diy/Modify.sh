1d95bbd1031de1553abd9676f48ef38523ec4633
有種 <39034242+kenzok8@users.noreply.github.com>
2022-04-14 10:15:24 +0800

    Update Modify.sh

--- diy/Modify.sh
+++ diy/Modify.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 # --------------------------------------------------------
 # Script for creating ACL file for each LuCI APP
-#!/bin/bash
 # --------------------------------------------------------
 sed -i \
 -e 's?include \.\./\.\./\(lang\|devel\)?include $(TOPDIR)/feeds/packages/\1?' \
 -e 's?2. Clash For OpenWRT?3. Applications?' \
 -e 's?\.\./\.\./luci.mk?$(TOPDIR)/feeds/luci/luci.mk?' \
+-e 's/ca-certificates/ca-bundle/' \
 */Makefile
 
 sed -i 's/luci-lib-ipkg/luci-base/g' luci-app-store/Makefile
+sed -i "/minisign:minisign/d" luci-app-dnscrypt-proxy2/Makefile
+sed -i 's/+dockerd/+dockerd +cgroupfs-mount/' luci-app-docker*/Makefile
+sed -i '$i /etc/init.d/dockerd restart &' luci-app-docker*/root/etc/uci-defaults/*
+sed -i 's/+libcap /+libcap +libcap-bin /' luci-app-openclash/Makefile
+sed -i 's/\(+luci-compat\)/\1 +luci-theme-argon/' luci-app-argon-config/Makefile
+sed -i 's/\(+luci-compat\)/\1 +luci-theme-argonne/' luci-app-argonne-config/Makefile
+sed -i 's/ +uhttpd-mod-ubus//' luci-app-packet-capture/Makefile
+sed -i 's/	ip.neighbors/	luci.ip.neighbors/' luci-app-wifidog/luasrc/model/cbi/wifidog/wifidog_cfg.lua
 
-cp -rf base-files/files/sbin/sysupgrade my-default-settings/files/sbin/
 bash diy/create_acl_for_luci.sh -a >/dev/null 2>&1
 bash diy/convert_translation.sh -a >/dev/null 2>&1
 
